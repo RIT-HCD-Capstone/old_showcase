cc77c853956ba134b85ab774422424dfe0bff7f2
destructionorb <neonepsd@gmail.com>
2022-11-04 11:39:14 -0400

    removed second footer and redirected home to search - j

--- wp-content/themes/openlab-theme-child/footer.php
+++ wp-content/themes/openlab-theme-child/footer.php
@@ -7,24 +7,11 @@
  *
  * @author OpenLab Team
  * @author Kadin Benjamin ktb1193
+ * @author josh clemens jc2182
  * */
 ?>
-					</div><!--end .container-->
-
-					<?php do_action( 'bp_after_container' ); ?>
-					<?php do_action( 'bp_before_footer' ); ?>
-
-					<?php do_action( 'bp_footer' ); ?>
-
-					<?php do_action( 'bp_after_footer' ); ?>
-
-					</div><!--.page-table-row-->
-
 					<?php site_footer(); //function defined in functions.php ?>
-
 					<?php wp_footer(); ?>
-
-				</div><!--.page-table-->
 
 	</body>
 
